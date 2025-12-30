a720f1b0ca6c2d4e880529618e7dd258f7829a4d
zazkenti <a123nce@gmail.com>
2025-12-29 22:10:09 -0800

    first commit

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import fetch from "node-fetch";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/quote/:symbol", async (req, res) => {
+  try {
+    const symbol = req.params.symbol;
+    const apikey = process.env.API_KEY;
+
+    const url = `https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${apikey}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    res.json(data);
+  } catch {
+    res.status(500).json({ error: "fetch failed" });
+  }
+});
+
+app.listen(3001, () => console.log("Server running on 3001"));
---
+++ frontend/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+function App() {
+  const [symbol, setSymbol] = useState("");
+  const [stock, setStock] = useState(null);
+
+  const getStock = async () => {
+    try {
+      const response = await fetch(`http://localhost:3001/api/quote/${symbol}`);
+      const data = await response.json();
+      setStock(data);
+    } catch (err) {
+      console.error("Error: ", err);
+    }
+  };
+
+  return (
+    <>
+      <h2>Stock App</h2>
+      <input
+        type="text"
+        value={symbol}
+        onChange={(e) => setSymbol(e.target.value)}
+        placeholder="Enter Symbol"
+      />
+
+      <button onClick={getStock}>Search</button>
+
+      {stock && (
+        <div>
+          <p>Open Price: {stock.o}</p>
+          <p>Current Price: {stock.c}</p>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
